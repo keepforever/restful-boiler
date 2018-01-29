188f6737d1e242b61045c74ecbd5c7a5c74f5fc4
Brian Cilenti <briancilenti@gmail.com>
2018-01-29 13:11:42 -0800

    finished orders prior to image upload feature

--- api/models/order.js
+++ api/models/order.js
@@ -11,3 +11,9 @@ const orderSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('Order', orderSchema);
+
+// in building the orderSchema we create a "relation" to
+// the productSchema via the 'ref' property in product object
+// above.
+// ref should hold a string with the name of the model you want
+// to connect THIS model to. 
